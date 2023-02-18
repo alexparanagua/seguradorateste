80d6e98aabcf8b048ad969522e74ce9da198dc84
Alex Paranaguá <alexparanagua@gmail.com>
2023-02-18 16:07:10 -0300

    init

---
+++ src/main/java/com/teste/seguradora/seguradorateste/SeguradoratesteApplication.java
@@ -0,0 +1,13 @@
+package com.teste.seguradora.seguradorateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguradoratesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguradoratesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/seguradora/seguradorateste/SeguradoratesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.seguradora.seguradorateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguradoratesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
